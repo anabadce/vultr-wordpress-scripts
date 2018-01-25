844adf8050a29c17e2e7146ec50c0e07548d5cfc
Angel Abad Cerdeira <angel@abadcer.com>
2018-01-25 11:32:48 +0000

    resilio script starts on boot and monthly updates

--- resilio.sh
+++ resilio.sh
@@ -18,7 +18,26 @@ fi
 tar -zxf resilio-sync_x64.tar.gz
 ./rslsync
 
+# cleaning
 rm -f $DOWNLOAD_FILE
 
+# Start on boot
+if [[ -f /etc/cron.d/resilio-sync ]]; then
+    echo "INFO: /etc/cron.d/resilio-sync already exist"
+else    
+    echo "@reboot root $INSTALL_PATH/rslsync" > /etc/cron.d/resilio-sync
+    echo "INFO: /etc/cron.d/resilio-sync created"
+fi
+
+# Update + restart every month
+if [[ -f /etc/cron.d/resilio-sync-update ]]; then
+    echo "INFO: /etc/cron.d/resilio-sync-update already exist"
+else
+    echo "@monthly root /root/vultr-wordpress-scripts/resilio.sh" > /etc/cron.d/resilio-sync-update
+    echo "INFO: /etc/cron.d/resilio-sync-update created"
+fi
+
+
+# Done
 popd
 
