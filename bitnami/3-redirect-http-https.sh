8aef1a179ca8221736a350eb57e6796b66b55210
Angel Abad Cerdeira <angel@abadcer.com>
2018-08-09 03:59:50 +0000

    Tweaks

--- bitnami/3-redirect-http-https.sh
+++ bitnami/3-redirect-http-https.sh
@@ -7,12 +7,19 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+if [[ -z $1 ]]; then
+    echo "Usage: $(basename "$0") DOMAIN_NAME"
+    exit 1
+else
+    DOMAIN_NAME=$1
+fi
+
 APACHE_CONF="/opt/bitnami/apache2/conf/bitnami/bitnami.conf"
 START_LINE="<VirtualHost _default_:80>"
 
 LINE1="RewriteEngine On"
 LINE2="RewriteCond %{HTTPS} !=on"
-LINE3="RewriteRule ^/(.*) https://%{SERVER_NAME}/$1 [R,L]"
+LINE3="RewriteRule ^/(.*) https://$DOMAIN_NAME/$1 [R,L]"
 
 if grep --quiet "$START_LINE" "$APACHE_CONF"; then
 
@@ -21,6 +28,7 @@ if grep --quiet "$START_LINE" "$APACHE_CONF"; then
     else
         echo "INFO: Adding HTTPS redirect"
         sed -i.bak "/$START_LINE/a $LINE1\n$LINE2\n$LINE3" $APACHE_CONF
+        /opt/bitnami/ctlscript.sh restart apache
     fi
 else
     echo "ERROR: Cannot find START_LINE"
