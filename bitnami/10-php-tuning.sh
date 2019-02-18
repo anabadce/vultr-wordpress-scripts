b19bee9edf9dcf172b5e2701147f1f593363d8c2
Angel <angel@abadcer.com>
2019-02-18 00:31:02 +0000

    adding script to tune php.ini

---
+++ bitnami/10-php-tuning.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+# Setting up cron jobs for updates and so on...
+
+if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: This script must be run as root"
+    exit 1
+fi
+
+PHP_PATH=/opt/bitnami/php/etc/php.ini
+
+# ======== php.ini ========
+
+if [[ -f "$PHP_PATH" ]] ; then
+    echo "INFO: Found php.ini, changing..."
+
+    sed -i.bak -E "s/.*max_input_vars\ =\ .*/max_input_vars = 3000/" "$PHP_PATH"
+
+    /opt/bitnami/ctlscript.sh restart php-fpm
+else
+    echo "ERROR: cannot find php.ini in $PHP_PATH ..."
+    exit 1
+fi
+
+echo "INFO: Done"
+
