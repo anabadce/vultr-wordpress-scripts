8da757372b15d8be97bc1af124ae660de821ecf4
Angel Abad Cerdeira <angel@abadcer.com>
2019-05-20 11:34:45 +0000

    un-update in datadog agent remove some devices

--- bitnami/7-datadog.sh
+++ bitnami/7-datadog.sh
@@ -12,24 +12,7 @@ else
     DATADOG_KEY=$1
 fi
 
-DD_CONF_PATH=/etc/datadog-agent/conf.d
 DD_API_KEY=$DATADOG_KEY bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
-
-cat <<EOT > $DD_CONF_PATH/disk.d/disk.yaml
-init_config:
-
-instances:
-  - use_mount: false
-
-mount_point_blacklist:
-  - devtmpfs
-  - tmpfs
-  - udev
-  - /dev/loop0
-  - /dev/loop1
-  - /dev/loop2
-  - /dev/loop3
-EOT
 
 echo "INFO: Done"
 echo "INFO: Run \"sudo datadog-agent status\" to check"
